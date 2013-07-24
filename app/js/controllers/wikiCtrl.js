714247b4eb326096239888a95bee9956f7826a6b
Brian Chen <brianchen8990@gmail.com>
2013-07-24 12:56:44 +0200

    add generate content list from data function in wiki controller

--- app/js/controllers/wikiCtrl.js
+++ app/js/controllers/wikiCtrl.js
@@ -1,9 +1,28 @@
 'use strict';
 
-app.controller('WikiCtrl',['$scope','$log','$state','$stateParams','WikiPageService',function($scope,$log,$state,$stateParams,WikiPageService){
+app.controller('WikiCtrl',['$scope','$log','$state','$stateParams','WikiPageService','UtilityService',function($scope,$log,$state,$stateParams,WikiPageService,UtilityService){
 	
 	function renderContentList(){
 		console.log('render content list');
+
+		var connect = UtilityService.internetConnection.checkConnection(navigator.connection.type);
+
+		if(connect == 'No network connection'){
+			console.log('no internet');
+			
+		}else{
+			WikiPageService.fetchMainNode($scope.currentGroup.id).then(function(rep){
+				console.log(rep);
+				WikiPageService.fetchWikiPages(WikiPageService.getMainNode().nodeId).then(function(rep){
+					console.log(rep);
+					$scope.listTree = WikiPageService.getWikiTree();
+				},function(err){
+					console.log(err);
+				});
+			},function(error){
+				console.log(error);
+			});
+		}
 	}
 
 	if($scope.currentGroup.id != 110 && $state.is('stage.wiki.contentlist')){
