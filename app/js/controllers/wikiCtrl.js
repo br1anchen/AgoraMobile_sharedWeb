58e60b07af49db13b7e610bb03d68df156b6ff2c
Brian Chen <brianchen8990@gmail.com>
2013-08-12 13:47:24 +0200

    remove console log message in loadexternallink function

--- app/js/controllers/wikiCtrl.js
+++ app/js/controllers/wikiCtrl.js
@@ -90,7 +90,6 @@ app.controller('WikiCtrl',['$scope','$log','$state','$stateParams','WikiPageServ
 	}
 
 	$scope.loadExternalLink = function(url){
-		console.log(url);
 		var ref = window.open(url, '_blank', 'location=yes');
         ref.addEventListener('loadstart', function() { console.log('start: ' + event.url); });
         ref.addEventListener('loadstop', function() { console.log('stop: ' + event.url); });
