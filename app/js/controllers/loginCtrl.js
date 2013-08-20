4c75380d6f5d7fa9dd2a4908bd1a392c9b8af795
Brian Chen <brianchen8990@gmail.com>
2013-08-20 11:49:28 +0200

    make new feide user choose domain select button on login page

--- app/js/controllers/loginCtrl.js
+++ app/js/controllers/loginCtrl.js
@@ -10,10 +10,20 @@ app.controller('LoginCtrl',function($scope,$log,LoginService,$state,$timeout,$ro
 		return rep.data + "&RelayState=%2Fgroup%2Fagora%2Fdokumenter%3Fp_p_id%3Dagoramypassword_WAR_agoramypasswordportlet%26p_p_state%3Dpop_up%26p_p_mode%3Dedit%26p_p_lifecycle0%26controlPanelCategory%253portlet_agoramypassword_WAR_agoramypasswordportlet";
 	});
 	
+	$scope.affiliations = LoginService.getAffiliation().then(function(rep){
+		return rep;
+	});
+
+	$scope.feideDomain = 'nodomain';
+
 	$scope.login = function(){
 
 			$scope.loginMsg = { type: 'success', msg: 'Login ...' };
 			$("#loginMessage").css("visibility", "visible");
+
+			if($scope.feideDomain != 'nodomain'){
+				$scope.username = $scope.username + '__' + $scope.feideDomain;
+			}
 
 			LoginService.login($scope.username,$scope.password).then(
 				function(rep){
