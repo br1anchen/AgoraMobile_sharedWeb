5d73fb7cd1e8bad9ad767933f99f889294b6014c
Brian Chen <brianchen8990@gmail.com>
2013-08-26 15:23:42 +0200

    add localization to app

---
+++ app/lib/localization/resources-locale_en-us.js
@@ -0,0 +1,12 @@
+[
+    {
+        "key":"_LoginNameTitle_",
+        "value":"Screen Name",
+        "description":"Login page username text"
+    },
+    {
+        "key":"_LoginPwdTitle_",
+        "value":"Password",
+        "description":"Login page password text"
+    }
+]
---
+++ app/lib/localization/resources-locale_nb-no.js
@@ -0,0 +1,12 @@
+[
+    {
+        "key":"_LoginNameTitle_",
+        "value":"Brukernavn",
+        "description":"Login page username text"
+    },
+    {
+        "key":"_LoginPwdTitle_",
+        "value":"Passord",
+        "description":"Login page password text"
+    }
+]
